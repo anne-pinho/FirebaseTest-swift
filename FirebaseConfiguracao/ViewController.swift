1ce87838dea5d87bde189b84b4c27da3f64ee065
anne-pinho <anne.pinhov@gmail.com>
2021-12-19 14:06:42 -0400

    praticand autenticacao, criar, logar e logout de usuario, e recuperar valores

--- FirebaseConfiguracao/ViewController.swift
+++ FirebaseConfiguracao/ViewController.swift
@@ -6,14 +6,71 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textoLabel: UILabel!
+    
+    let firebase = Database.database().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //criar usuario
+//        Auth.auth().createUser(withEmail: "msantosjck@gmail.com", password: "anne123456") { usuario, erro in
+//            if erro == nil { //sucesso
+//                print("sucesso ao cadastrar usuario")
+//            } else { //erro
+//                print("erro ao cadastrar usuario : \(String(describing: erro?.localizedDescription))")
+//
+//            }
+//        }
+//
+        
+        //logar usuario
+        Auth.auth().signIn(withEmail: "msantosjck@gmail.com", password: "anne123456") { usuario, erro in
+            if erro == nil { //sucesso
+                print("sucesso ao logar usuario")
+            } else { //erro
+                print("erro ao logar usuario: \(String(describing: erro?.localizedDescription))")
+
+            }
+        }
+        
+        
+        //autenticacao
+        Auth.auth().addStateDidChangeListener({ Auth, usuario in
+            if let usuarioLogado = usuario {
+                print("usuario logado" + String(describing: usuarioLogado.email))
+
+            } else {
+                print("usuario nao logado")
+            }
+        })
+        
+        
+        
+                
+        //Login e logout de usuario
+//        do{
+//            try Auth.auth().signOut()
+//        } catch {
+//            print("erro ao deslogar usuario")
+//        }
+//
+        
+        
+
+        
+        //criar um nó no banco e recuperar valor
+//        let usuario = firebase.child("usuario").child("nome")
+//        //usuario.child("nome").setValue("carol")
+//
+//        usuario.observe(DataEventType.value, with: {( dados ) in
+//            let nome = dados.value as! String
+//            self.textoLabel.text = nome
+//        })
     }
-
-
 }
 
